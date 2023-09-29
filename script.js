9078bf9b7945c48f1df40acaa6c4aaa9cd05769f
Ramon Oliveira <ramon.ubt@gmail.com>
2023-09-28 22:43:45 -0300

    refatorando

--- script.js
+++ script.js
@@ -2,46 +2,49 @@ const colorPallete = document.getElementById('color-palette');
 const colors = colorPallete.children;
 const pixelBoard = document.getElementById('pixel-board');
 const numberOfPixels = 25;
-const colorSelectedVar = document.querySelector('.selected');
 
 const selectColor = (element) => {
   for (let i = 0; i < colors.length; i += 1) {
     if (colors[i].classList.contains('selected')) {
       colors[i].className = 'color';
-      colors[i].innerText = ''
+      colors[i].innerText = '';
     }
   }
-  element.className = 'color selected';
-  element.innerHTML = '&checkmark;';
-}
+  const newElement = element;
+
+  newElement.className = 'color selected';
+  newElement.innerHTML = '&checkmark;';
+};
 
 for (let i = 0; i < colors.length; i += 1) {
-  const colorSelected = () => {selectColor(colors[i]);}
-  colors[i].addEventListener("click", colorSelected);
-  colors[i].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
+  const colorSelected = () => { selectColor(colors[i]); };
+  colors[i].addEventListener('click', colorSelected);
+  const numbers = [Math.floor(Math.random() * 255) + 1, Math.floor(Math.random() * 255) + 1, Math.floor(Math.random() * 255) + 1];
+  colors[i].style.backgroundColor = `rgb(${numbers[0]}, ${numbers[1]}, ${numbers[2]})`;
 }
 
 const changeColor = (target) => {
-  target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-}
+  const newTarget = target;
+  newTarget.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+};
 
 for (let i = 0; i < numberOfPixels; i += 1) {
-  let pixel = document.createElement('div');
+  const pixel = document.createElement('div');
   pixel.className = 'pixel';
-  const colorChange = _ => (changeColor(pixel));
-  pixel.addEventListener('click', colorChange)
+  const colorChange = () => (changeColor(pixel));
+  pixel.addEventListener('click', colorChange);
   pixelBoard.appendChild(pixel);
 }
 
-const cleanBoard = _ => {
+const cleanBoard = () => {
   let pixels = document.getElementsByClassName('pixel');
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = '#FFF';
   }
 }
 
-const changePalette = _ => {
+const changePalette = () => {
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
 }
-}
+};
