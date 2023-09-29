e399bd6a54259ea23c588c4ec0fe754cb3ba9fba
Ramon Oliveira <ramon.ubt@gmail.com>
2023-09-29 15:01:26 -0300

    finalizando requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const colorPallete = document.getElementById('color-palette');
 const colors = colorPallete.children;
 const pixelBoard = document.getElementById('pixel-board');
@@ -31,16 +32,10 @@ const changeColor = (target) => {
 for (let i = 0; i < numberOfPixels; i += 1) {
   const pixel = document.createElement('div');
   pixel.className = 'pixel';
+  pixel.style.backgroundColor = 'white';
   const colorChange = () => (changeColor(pixel));
   pixel.addEventListener('click', colorChange);
   pixelBoard.appendChild(pixel);
-}
-
-const cleanBoard = () => {
-  let pixels = document.getElementsByClassName('pixel');
-  for (let i = 0; i < pixels.length; i += 1) {
-    pixels[i].style.backgroundColor = '#FFF';
-  }
 }
 
 const changePalette = () => {
@@ -48,3 +43,39 @@ const changePalette = () => {
     colors[i].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
 }
 };
+
+const pixelItems = document.getElementsByClassName('pixel');
+const colorsStoraged = JSON.parse(localStorage.getItem('pixelBoard')) || [];
+
+const updateColor = (index, arr) => {
+  colorsStoraged[index] = arr[index].style.backgroundColor;
+  localStorage.setItem('pixelBoard', JSON.stringify(colorsStoraged));
+};
+
+for (let i = 0; i < pixelItems.length; i += 1) {
+  const updtColor = () => (updateColor(i, pixelItems));
+  pixelItems[i].addEventListener('click', updtColor);
+}
+
+const setPixelStrgd = (index, color) => {
+  pixelItems[index].style.backgroundColor = color;
+};
+
+const loadColor = () => {
+  for (let i = 0; i < pixelItems.length; i += 1) {
+    setPixelStrgd(i, colorsStoraged[i]);
+  }
+};
+
+const cleanBoard = () => {
+  for (let i = 0; i < colorsStoraged.length; i += 1) {
+    colorsStoraged[i] = 'white';
+  }
+  localStorage.setItem('pixelBoard', JSON.stringify(colorsStoraged));
+  let pixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = '#FFF';
+  }
+};
+
+loadColor();
